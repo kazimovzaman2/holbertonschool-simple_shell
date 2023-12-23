c67e063ac43bd1d67b356c12c5dadf3e68f26ff3
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-22 23:44:01 -0800

    Execute Command functionality writed.

--- execute_command.c
+++ execute_command.c
@@ -3,5 +3,25 @@
 
 void execute_command(char **args)
 {
-	pid_t
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		perror("fork");
+	}
+	else if (child_pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("Error:");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
 }
