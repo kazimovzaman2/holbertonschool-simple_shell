911b98e40bcabd467d6829e7cefbd17268579f8a
Kamalusta <kamalusta@gmail.com>
2023-12-26 05:11:22 -0800

    memory leak

--- execute_command.c
+++ execute_command.c
@@ -9,76 +9,76 @@
  */
 void execute_command(char **args)
 {
-	pid_t child_pid;
-	int status, i = 0;
-	char *fullPath;
-	struct stat st;
-	char **pathArr;
- 	char *path = malloc(strlen(getenv("PATH")) * sizeof(char *));
- 	strcpy(path, getenv("PATH"));
- 	pathArr = parse_line(path, ":");
-	if (!path || !pathArr)
-	  perror("malloc");
- 	free(path);
+  pid_t child_pid;
+    int status, i = 0;
+    char *fullPath;
+    struct stat st;
+    char **pathArr;
+    char *path = malloc(strlen(getenv("PATH")) * sizeof(char *));
+    strcpy(path, getenv("PATH"));
+    pathArr = parse_line(path, ":");
+    if (!path || !pathArr)
+    perror("malloc");
+    free(path);
 
-	if (stat(args[0], &st) == 0)
+    if (stat(args[0], &st) == 0)
     {
         fullPath = malloc(strlen(args[0]) * sizeof(char *));
         strcpy(fullPath, args[0]);
         child_pid = fork();
     }
-    else  
+    else
     {
-	    while(pathArr[i])
-	    {
-	        fullPath = malloc((strlen(pathArr[i]) + strlen(args[0]) + 1) * sizeof(char *));
- 	        strcpy(fullPath, pathArr[i]);
- 	        strcat(fullPath, "/");
- 	        strcat(fullPath, args[0]);
-	        if (stat(fullPath, &st) == 0)
+        while(pathArr[i])
+        {
+            fullPath = malloc((strlen(pathArr[i]) + strlen(args[0]) + 1) * sizeof(char *));
+            strcpy(fullPath, pathArr[i]);
+            strcat(fullPath, "/");
+            strcat(fullPath, args[0]);
+            if (stat(fullPath, &st) == 0)
             {
-            child_pid = fork();
-            break;
+                child_pid = fork();
+                break;
             }
             free(fullPath);
             i++;
-    	}
+        }
     }
-	if(!pathArr[i])
-	{
-	  for (i = 0; pathArr[i]; i++)
-	    free(pathArr[i]);
-	  free(pathArr);
-	  perror("Error");
-	}
-	else
-	{
-	     if (child_pid == -1)
-	    {
-	        free(fullPath);
-		for (i = 0; pathArr[i]; i++)
-	            free(pathArr[i]);
-	        free(pathArr);
-		perror("fork");
-	    }
-	    else if (child_pid == 0)
-	    {
-		    if (execve(fullPath, args, NULL) == -1)
-		    {
-		      free(fullPath);
-		      for (i = 0; pathArr[i]; i++)
-	            free(pathArr[i]);
-	        free(pathArr);
-		      perror("Error");
-		      exit(EXIT_FAILURE);
-		    }
-	    }
-	    else
-		    wait(&status);
-	     
-	     free(fullPath);
-	     for (i = 0; pathArr[i]; i++)
-	            free(pathArr[i]);
-	        free(pathArr);
-	}
+    if(!pathArr[i])
+    {
+        for (i = 0; pathArr[i]; i++)
+        free(pathArr[i]);
+        free(pathArr);
+        perror("Error");
+    }
+    else
+    {
+        if (child_pid == -1)
+        {
+            free(fullPath);
+            for (i = 0; pathArr[i]; i++)
+            free(pathArr[i]);
+            free(pathArr);
+            perror("fork");
+        }
+        else if (child_pid == 0)
+        {
+            if (execve(fullPath, args, NULL) == -1)
+            {
+                free(fullPath);
+                for (i = 0; pathArr[i]; i++)
+                free(pathArr[i]);
+                free(pathArr);
+                perror("Error");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        wait(&status);
+
+        free(fullPath);
+        for (i = 0; pathArr[i]; i++)
+        free(pathArr[i]);
+        free(pathArr);
+    }
 }
